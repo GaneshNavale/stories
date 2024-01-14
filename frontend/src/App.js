b4cb4d822403bbfdaadbf22a05e4e97d364389d8
Ganesh Navale <navaleganesh2011@gmail.com>
2024-01-14 12:35:11 +0530

    Initial Commit
    # Conflicts:
    #	frontend/package-lock.json
    #	frontend/package.json
    #	frontend/src/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import List from "@editorjs/list";
+import { useEffect, useRef, useState } from "react";
+import SimpleImage from "simple-image-editorjs";
+import { Parser, Table } from "@alkhipce/editorjs-react";
 
 function App() {
+  const ejInstance = useRef();
+  const [editorContent, setEditorContent] = useState({});
+
+  const DEFAULT_INITIAL_DATA = {
+    time: new Date().getTime(),
+    blocks: [
+      {
+        type: "header",
+        data: {
+          text: "This is my awesome editor!",
+          level: 1,
+        },
+      },
+    ],
+  };
+
+  const initEditor = () => {
+    const editor = new EditorJS({
+      /**
+       * Id of Element that should contain Editor instance
+       */
+      holder: "editorjs",
+      placeholder: "Let`s write an awesome story!",
+      autofocus: false,
+      data: {},
+      onReady: () => {
+        console.log("Editor.js is ready to work!");
+      },
+      onChange: async () => {
+        let content = await editor.saver.save();
+
+        console.log(content);
+        setEditorContent(content);
+      },
+      tools: {
+        heading: {
+          class: Header,
+          config: {
+            placeholder: "Header",
+          },
+          shortcut: "CMD+SHIFT+H",
+        },
+        list: List,
+        image: {
+          class: SimpleImage,
+          inlineToolbar: true,
+          config: {
+            placeholder: "Paste image URL",
+          },
+        },
+      },
+    });
+  };
+
+  useEffect(() => {
+    if (ejInstance.current === null) {
+      initEditor();
+    }
+
+    return () => {
+      ejInstance?.current?.destroy();
+      ejInstance.current = null;
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="editorjs"></div>
+      {/* <Parser data={editorContent} /> */}
+    </>
   );
 }
 
